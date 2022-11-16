815c395607019e68083ad06b8c4c064d1ab5ab05
Wolf <d.wolftripp@gmail.com>
2022-11-16 11:16:10 -0800

    populate header.php, remove repeated code, add nav links to header

---
+++ inc/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Email Vault</title>
+</head>
+<body>
+<nav>
+  <div>
+    <ul>
+      <li>
+        <a href="/email-vault/index.php">Home</a>
+      </li>
+      <li>
+        <a href="/email-vault/view.php">Emails</a>
+      </li>
+    </ul>
+  </div>
+</nav>
