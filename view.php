61853eee2f19ad68fb9a32f5597b320c2a218b64
Wolf <d.wolftripp@gmail.com>
2022-11-16 11:19:35 -0800

    add template code

--- view.php
+++ view.php
@@ -1,3 +1,9 @@
 <?php include'inc/header.php'; ?>
 
 <h1>Emails Go Here</h1>
+
+<div class="email-list">
+  <p class="email-list-body">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore molestias eum dolore iste deserunt cupiditate nisi assumenda est asperiores debitis. Impedit commodi, veritatis beatae maxime hic quos voluptate necessitatibus quas!
+  </p>
+</div>
