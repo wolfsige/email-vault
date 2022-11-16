0bf483653f565a75e89850c9a60ef905e4f14da4
Wolf <d.wolftripp@gmail.com>
2022-11-16 11:04:10 -0800

    add inputs for email and name

--- index.php
+++ index.php
@@ -8,5 +8,15 @@
 </head>
 <body>
   <h1>Emails Go Here</h1>
+  <form action="">
+    <div>
+      <label for="name">Name:</label></br>
+      <input type="text" placeholder="Enter Your Name">
+    </div>
+    <div>
+      <label for="email">Email:</label></br>
+      <input type="email" placeholder="Enter Your Email">
+    </div>
+  </form>
 </body>
 </html>
