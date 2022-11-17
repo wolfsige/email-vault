e320017a5980bdbd63451d9678e73adf4ce4cf87
Wolf <d.wolftripp@gmail.com>
2022-11-16 23:03:10 -0800

    display emails and names from databse

--- view.php
+++ view.php
@@ -1,9 +1,24 @@
 <?php include'inc/header.php'; ?>
 
+<?php 
+  $sql = 'SELECT * FROM `vault-data`';
+  $result = mysqli_query($conn, $sql);
+  $emails = mysqli_fetch_all($result, MYSQLI_ASSOC);
+?>
+
 <h1>Emails Go Here</h1>
 
+<?php if(empty($emails)): ?>
+  <p>There are no emails here!</p>
+<?php endif; ?>
+
+<?php foreach($emails as $item): ?>
 <div class="email-list">
   <p class="email-list-body">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore molestias eum dolore iste deserunt cupiditate nisi assumenda est asperiores debitis. Impedit commodi, veritatis beatae maxime hic quos voluptate necessitatibus quas!
+    <?php echo $item['email']; ?>
+    <div>
+      Owner: <?php echo $item['name']; ?>
+    </div>
   </p>
 </div>
+<?php endforeach; ?>
