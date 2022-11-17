02c8b2868189b21c8463a716a85d73d2ea2e13de
Wolf <d.wolftripp@gmail.com>
2022-11-17 00:28:07 -0800

    add submit method to database

--- index.php
+++ index.php
@@ -1,16 +1,48 @@
 <?php include'inc/header.php'; ?>
 
+<?php
+  $name = $email = '';
+  $nameErr = $emailErr = '';
+
+  if(isset($_POST['submit'])){
+    if(empty($_POST['name'])){
+      $nameErr = 'A Name is Required';
+    } else {
+      $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
+    }
+  
+
+    if(empty($_POST['email'])){
+      $emailErr = 'An Email is Required';
+    } else {
+      $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+    }
+
+    if(empty($nameErr && $emailErr)){
+      $sql = "INSERT INTO `vault-data` (name, email) VALUES('$name', '$email')";
+    }
+
+    if(mysqli_query($conn, $sql)){
+      header('Location: view.php');
+    } else {
+      echo 'Error: ' . mysqli_error($conn);
+    }
+  }
+?>
+
   <h1>Emails Go Here</h1>
-  <form action="">
+  <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
     <div>
       <label for="name">Name:</label></br>
-      <input type="text" placeholder="Enter Your Name">
+      <input type="text" id="name" name="name" placeholder="Enter Your Name"></br>
+      <?php echo $nameErr; ?>
     </div>
     <div>
       <label for="email">Email:</label></br>
-      <input type="email" placeholder="Enter Your Email">
+      <input type="email" id="email" name="email" placeholder="Enter Your Email"></br>
+      <?php echo $emailErr; ?>
     </div>
-    <button>Submit</button>
+    <input type="submit" name="submit" value="Send">
   </form>
 </body>
 </html>
