b34a9bd872f2c0d261d70f796c1530742f7ae8d5
Wolf <d.wolftripp@gmail.com>
2022-11-18 02:58:28 -0800

    fix indentation

--- index.php
+++ index.php
@@ -2,48 +2,47 @@
 <link rel="stylesheet" href="style/index-style.css">
 
 <?php
-  $name = $email = '';
-  $nameErr = $emailErr = '';
+    $name = $email = '';
+    $nameErr = $emailErr = '';
 
-  if(isset($_POST['submit'])){
-    if(empty($_POST['name'])){
-      $nameErr = 'A Name is Required';
-    } else {
-      $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
-    }
+    if(isset($_POST['submit'])){
+        if(empty($_POST['name'])){
+            $nameErr = 'A Name is Required';
+        } else {
+            $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
+        }
   
+      if(empty($_POST['email'])){
+          $emailErr = 'An Email is Required';
+      } else {
+          $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+      }
 
-    if(empty($_POST['email'])){
-      $emailErr = 'An Email is Required';
-    } else {
-      $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
-    }
-
-    if(empty($nameErr && $emailErr)){
-      $sql = "INSERT INTO `vault-data` (name, email) VALUES('$name', '$email')";
-    }
+      if(empty($nameErr && $emailErr)){
+          $sql = "INSERT INTO `vault-data` (name, email) VALUES('$name', '$email')";
+      }
   }
 ?>
 
-  <h1>Add a new Name and Email!</h1>
-  <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
+<h1>Add a new Name and Email!</h1>
+<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
     <div class="body">
-      <div class="text">
-        <label for="name">Name:</label></br>
-        <input type="text" class="input" id="name" name="name" placeholder="Enter Your Name"></br>
-        <div class="err">
-          <?php echo $nameErr; ?>
+        <div class="text">
+            <label for="name">Name:</label></br>
+            <input type="text" class="input" id="name" name="name" placeholder="Enter Your Name"></br>
+            <div class="err">
+                <?php echo $nameErr; ?>
+            </div>
+         </div>
+        <div class="text">
+            <label for="email">Email:</label></br>
+            <input type="email" class="input" id="email" name="email" placeholder="Enter Your Email"></br>
+            <div class="err">
+                <?php echo $emailErr; ?>
+            </div>
         </div>
-      </div>
-      <div class="text">
-        <label for="email">Email:</label></br>
-        <input type="email" class="input" id="email" name="email" placeholder="Enter Your Email"></br>
-        <div class="err">
-          <?php echo $emailErr; ?>
-        </div>
-      </div>
-      <input type="submit" name="submit" value="Submit" class="submit">
+        <input type="submit" name="submit" value="Submit" class="submit">
     </div>
-  </form>
+</form>
 </body>
 </html>
--- view.php
+++ view.php
@@ -2,26 +2,26 @@
 <link rel="stylesheet" href="style/email-style.css">
 
 <?php 
-  $sql = 'SELECT * FROM `vault-data`';
-  $result = mysqli_query($conn, $sql);
-  $emails = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    $sql = 'SELECT * FROM `vault-data`';
+    $result = mysqli_query($conn, $sql);
+    $emails = mysqli_fetch_all($result, MYSQLI_ASSOC);
 ?>
 
 <h1>All Emails</h1>
 
 <?php if(empty($emails)): ?>
-  <p>There are no emails here!</p>
+    <p>There are no emails here!</p>
 <?php endif; ?>
 
 <?php foreach($emails as $item): ?>
-<div class="cards">
-  <div class="email-list">
-    <p>
-      <div>
-        <?php echo $item['email']; ?></br>
-        Owner: <?php echo $item['name']; ?>
-      </div>
-    </p>
-  </div>
-</div>
+    <div class="cards">
+        <div class="email-list">
+            <p>
+               <div>
+                  <?php echo $item['email']; ?></br>
+                  Owner: <?php echo $item['name']; ?>
+               </div>
+            </p>
+        </div>
+    </div>
 <?php endforeach; ?>
