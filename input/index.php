f01d23411b6697167cac45611aec503bf75e5570
Wolf <d.wolftripp@gmail.com>
2022-11-26 03:13:26 -0800

    fix redirect after submit

--- input/index.php
+++ input/index.php
@@ -22,7 +22,7 @@
           $sql = "INSERT INTO `vault-data` (name, email) VALUES('$name', '$email')";
 
           if(mysqli_query($conn, $sql)){
-            header('Location: view.php');
+            header('Location: http://localhost/email-vault/emails/');
           } else {
             echo 'Error' . mysqli_error($conn);
           }
